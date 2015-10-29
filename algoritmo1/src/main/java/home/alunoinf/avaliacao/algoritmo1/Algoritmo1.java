d5d367971d1304eb3a7a9888493c164c7584f36c
alunoinf <alunoinf@pc-649301.(none)>
2015-10-29 20:19:40 -0200

    Algoritmo 1

---
+++ algoritmo1/src/main/java/home/alunoinf/avaliacao/algoritmo1/Algoritmo1.java
@@ -0,0 +1,27 @@
+package home.alunoinf.avaliacao.algoritmo1;
+
+/**
+ * Classe que retorna dia da semana usando uma data de entrada (dd/mm/aaaa);
+ */
+public class Algoritmo1 {
+    /**
+     *
+     * @param d Dia;
+     * @param m Mês;
+     * @param a Ano;
+     * @return Dia da semana da data de entrada;
+     */
+    public static int algoritmo1(int d, int m, int a){
+        if (d < 1 || d > 31 || m < 1 || m> 12 || a < 1753) {
+            throw new IllegalArgumentException("Data inválida");
+        }
+
+        if(m == 1 || m == 2){
+            m += 12;
+            a--;
+        }
+
+        int s = d + (2 * m) + (3 * (m - 1) / 5) + a + (a / 4) - (a / 100) + (a / 400);
+        return (s % 7);
+    }
+}
---
+++ algoritmo1/src/test/java/home/alunoinf/avaliacao/algoritmo1/Algoritmo1Test.java
@@ -0,0 +1,43 @@
+package home.alunoinf.avaliacao.algoritmo1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Algoritmo1Test {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testPrimeiroParametroDiaInvalido() throws Exception {
+        Algoritmo1.algoritmo1(0, 2, 2015);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSegundoParametroDiaInvalido() throws Exception {
+        Algoritmo1.algoritmo1(33, 5, 1910);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testTerceiroParametroMesInvalido() throws Exception {
+        Algoritmo1.algoritmo1(30, -5, 1910);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testQuartoParametroMesInvalido() throws Exception {
+        Algoritmo1.algoritmo1(15, 13, 2015);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testQuintoParametroAnoInvalido() throws Exception {
+        Algoritmo1.algoritmo1(30, 5, 1010);
+    }
+
+    @Test
+    public void testCasoTrivial() {
+        Assert.assertEquals(5, Algoritmo1.algoritmo1(25, 10, 2015));
+        Assert.assertEquals(6, Algoritmo1.algoritmo1(26, 10, 2015));
+        Assert.assertEquals(0, Algoritmo1.algoritmo1(27, 10, 2015));
+        Assert.assertEquals(1, Algoritmo1.algoritmo1(28, 10, 2015));
+        Assert.assertEquals(2, Algoritmo1.algoritmo1(29, 10, 2015));
+        Assert.assertEquals(3, Algoritmo1.algoritmo1(30, 10, 2015));
+        Assert.assertEquals(4, Algoritmo1.algoritmo1(31, 10, 2015));
+    }
+}
