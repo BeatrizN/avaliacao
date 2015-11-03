fabc952e54817ac1389c8096b8a734a8e096a174
alunoinf <alunoinf@pc-649301.(none)>
2015-11-03 16:40:24 -0200

    Algoritmo 10

---
+++ algoritmo10/src/main/java/home/alunoinf/avaliacao/algoritmo10/Algoritmo10.java
@@ -0,0 +1,41 @@
+package home.alunoinf.avaliacao.algoritmo10;
+
+/**
+ * Classe para implementação do crivo de Eratóstenes.
+ */
+public class Algoritmo10 {
+
+    /**
+     * Classe para implementação do crivo de Eratóstenes.
+     * Se o número for primo recebe o valor 0, senão recebe o valor 1.
+     * @param a Vetor de inteiros.
+     * @param n Numeros de termos do vetor.
+     */
+    public static void algoritmo10(int[] a, int n) {
+        if (n < 2) {
+            throw new IllegalArgumentException("Número menor que dois");
+        }
+        if (a.length < (n + 1)) {
+            throw new IllegalArgumentException("Tamanho do vetor");
+        }
+        for (int i = 2; i <= n; i++) {
+            if (a[i] != 0) {
+                throw new IllegalArgumentException("Número menor que zero");
+            }
+        }
+
+        int i = 2;
+        int c = 0;
+        while(i <= n/2) {
+            if (a[i] == 0) {
+                c = i + i;
+                while (c <= n) {
+                    a[c] = 1;
+                    c += i;
+                }
+            }
+
+            i++;
+        }
+    }
+}
