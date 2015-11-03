5338618d8afb906204901f45b85825eb07e96d3a
alunoinf <alunoinf@pc-649301.(none)>
2015-11-03 15:20:56 -0200

    Algoritmo 5

---
+++ algoritmo5/src/main/java/home/alunoinf/avaliacao/algoritmo5/Algoritmo5.java
@@ -0,0 +1,28 @@
+package home.alunoinf.avaliacao.algoritmo5;
+
+/**
+ * Classe para calcular potencia de números naturais.
+ */
+public class Algoritmo5 {
+
+    /**
+     * Classe para calcular potencia de números naturais utilizando mutiplicacao.
+     * @param x Base.
+     * @param y Expoente.
+     * @return Potencia x^y
+     */
+    public static int algoritmo5(int x, int y){
+        if ((x <= 0) || (y < 0)) {
+            throw new IllegalArgumentException("Número inválido");
+        }
+
+        int i = 1;
+        int p = 1;
+        while (i <= y) {
+            p = p * x;
+            i++;
+        }
+
+        return p;
+    }
+}
---
+++ algoritmo5/src/test/java/home/alunoinf/avaliacao/algoritmo5/Algoritmo5Test.java
@@ -0,0 +1,24 @@
+package home.alunoinf.avaliacao.algoritmo5;
+
+        import org.junit.Assert;
+        import org.junit.Test;
+
+public class Algoritmo5Test {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testPrimeiroParametroInvalido() throws Exception {
+        Algoritmo5.algoritmo5(0, 10);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testsegundoParametroInvalido() throws Exception {
+        Algoritmo5.algoritmo5(1, -10);
+    }
+
+    @Test
+    public void testCasoTrivial() {
+        Assert.assertEquals(100000, Algoritmo5.algoritmo5(10, 5));
+        Assert.assertEquals(49, Algoritmo5.algoritmo5(7, 2));
+        Assert.assertEquals(81, Algoritmo5.algoritmo5(3, 4));
+    }
+}
