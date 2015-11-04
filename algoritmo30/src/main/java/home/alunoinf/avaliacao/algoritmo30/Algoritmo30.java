5731ca3a70fb96c0fa78308c80eac40e648e21cd
alunoinf <alunoinf@pc-649301.(none)>
2015-11-04 16:05:42 -0200

    Algoritmo 30

---
+++ algoritmo30/src/main/java/home/alunoinf/avaliacao/algoritmo30/Algoritmo30.java
@@ -0,0 +1,44 @@
+package home.alunoinf.avaliacao.algoritmo30;
+/**
+ * Classe para calcular polinomios.
+ */
+
+public class Algoritmo30 {
+
+    /**
+     * Classe para calcular polinomios.
+     * p(x) = a[n] * x^n + a[n-1] * x^(n-1) + ... + a[2] * x^2 + a[1] * x + a[0].
+     * @param x Número.
+     * @param g Número inteiro.
+     * @param a Vetor.
+     * @return Polinomio.
+     */
+    public static int algoritmo30(int x, int g, int[] a) {
+        if ((g < 1) || (a[g] == 0)) {
+            throw new IllegalArgumentException("g menor que zero ou a igual a zero");
+        }
+        int p = a[0];
+        int i = 1;
+        int t = potencia(x, g);
+
+        while (i <= g) {
+            t = potencia(x, i);
+            p = (p + a[i] * t);
+            i += 1;
+        }
+        return p;
+    }
+
+    public static int potencia(int x, int y) {
+        int i = 1;
+        int p = 1;
+
+        while (i <= y) {
+            p = p * x;
+            i++;
+        }
+
+        return p;
+    }
+}
+
