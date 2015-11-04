634e0ccb11bc7a1b22ed8787e30f93e676372ce5
alunoinf <alunoinf@pc-649301.(none)>
2015-11-04 15:37:15 -0200

    Algoritmo 27

---
+++ algoritmo27/src/main/java/home/alunoinf/avaliacao/algoritmo27/Algoritmo27.java
@@ -0,0 +1,25 @@
+package home.alunoinf.avaliacao.algoritmo27;
+/**
+ *Classe que retorna o resto  da divisão de dois inteiros utilizando subtração.
+ */
+public class Algoritmo27 {
+
+    /**
+     * Classe que retorna o resto  da divisão de dois números inteiros utilizando subtração.
+     * @param x Dividendo.
+     * @param y Divisor.
+     * @return Resto.
+     */
+    public static long algoritmo27(int x, int y) {
+        if ((x <= 0) || (y < 0)) {
+            throw new IllegalArgumentException("Número inválido");
+        }
+
+        int s = x;
+        while(s >= y) {
+            s -= y;
+        }
+
+        return s;
+    }
+}
---
+++ algoritmo27/src/test/java/home/alunoinf/avaliacao/algoritmo27/Algoritmo27Test.java
@@ -0,0 +1,24 @@
+package home.alunoinf.avaliacao.algoritmo27;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Algoritmo27Test {
+    @Test(expected = IllegalArgumentException.class)
+    public void testPrimeiroParametroInvalido() throws Exception {
+        Algoritmo27.algoritmo27(0, 33);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSegundoParametroInvalido() throws Exception {
+        Algoritmo27.algoritmo27(62, -3);
+    }
+    
+    @Test
+    public void testCasoTrivial() {
+        Assert.assertEquals(0, Algoritmo27.algoritmo27(60, 2));
+        Assert.assertEquals(1, Algoritmo27.algoritmo27(9, 2));
+        Assert.assertEquals(3, Algoritmo27.algoritmo27(48, 5));
+        Assert.assertEquals(2, Algoritmo27.algoritmo27(10, 8));
+    }
+}
