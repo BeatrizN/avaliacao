fab1b558b79e6716949d36bbbd339d46079a44f1
alunoinf <alunoinf@pc-649301.(none)>
2015-11-05 19:49:46 -0200

    Algoritmo 3

--- algoritmo3/src/main/java/home/alunoinf/avaliacao/algoritmo3/Algoritmo3.java
+++ algoritmo3/src/main/java/home/alunoinf/avaliacao/algoritmo3/Algoritmo3.java
@@ -10,13 +10,14 @@ public class Algoritmo3 {
      * @param n número natural.
      * @return soma.
      */
-    public static int algoritmo3(int n){
+    public static int algoritmo3(int n) {
         if (n < 1) {
             throw new IllegalArgumentException("Número inválido");
         }
 
         int i = 2;
         int s = 1;
+
         while (i <= n) {
             s += i;
             i++;
