1f146159c23d1a5f0181a935979b5e35f4e1c7d5
alunoinf <alunoinf@pc-649301.(none)>
2015-11-05 19:35:59 -0200

    Algoritmo 1

--- algoritmo1/src/main/java/home/alunoinf/avaliacao/algoritmo1/Algoritmo1.java
+++ algoritmo1/src/main/java/home/alunoinf/avaliacao/algoritmo1/Algoritmo1.java
@@ -1,22 +1,23 @@
 package home.alunoinf.avaliacao.algoritmo1;
 
 /**
- * Classe que retorna dia da semana usando uma data de entrada (dd/mm/aaaa);
+ * Classe que retorna dia da semana usando uma data de entrada (dd/mm/aaaa).
  */
 public class Algoritmo1 {
+    
     /**
-     *
-     * @param d Dia;
-     * @param m Mês;
-     * @param a Ano;
-     * @return Dia da semana da data de entrada;
+     * Classe que retorna dia da semana usando uma data de entrada (dd/mm/aaaa).
+     * @param d Dia.
+     * @param m Mês.
+     * @param a Ano.
+     * @return Dia da semana da data de entrada.
      */
-    public static int algoritmo1(int d, int m, int a){
+    public static int algoritmo1(int d, int m, int a) {
         if (d < 1 || d > 31 || m < 1 || m> 12 || a < 1753) {
             throw new IllegalArgumentException("Data inválida");
         }
 
-        if(m == 1 || m == 2){
+        if(m == 1 || m == 2) {
             m += 12;
             a--;
         }
