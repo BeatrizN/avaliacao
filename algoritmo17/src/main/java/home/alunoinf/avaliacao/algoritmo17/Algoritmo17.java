8698b481415f728ff6067d38ea27102db56e3466
alunoinf <alunoinf@pc-649301.(none)>
2015-11-06 21:27:05 -0200

    Algoritmo 17

--- algoritmo17/src/main/java/home/alunoinf/avaliacao/algoritmo17/Algoritmo17.java
+++ algoritmo17/src/main/java/home/alunoinf/avaliacao/algoritmo17/Algoritmo17.java
@@ -10,13 +10,14 @@ public class Algoritmo17 {
      * @param n Número natural.
      * @return Fatorial de n.
      */
-    public static int algoritmo17(int n){
+    public static int algoritmo17(int n) {
         if (n < 1) {
             throw new IllegalArgumentException("Número inválido");
         }
 
         int i = 2;
         int f = 1;
+
         while (i <= n) {
             f = produto(f, i);
             i++;
@@ -26,8 +27,16 @@ public class Algoritmo17 {
     }
 
     public static int produto(int a, int b) {
+
+        /**
+         * Classe para calcular produto de dois numeros naturais utilizando soma.
+         * @param a primeiro fator.
+         * @param b segundo fator.
+         * @return Produto de a e b.
+         */
         int i = 1;
         int s = 0;
+
         while (i <= b) {
             s += a;
             i++;
--- algoritmo17/src/test/java/home/alunoinf/avaliacao/algoritmo17/Algoritmo17Test.java
+++ algoritmo17/src/test/java/home/alunoinf/avaliacao/algoritmo17/Algoritmo17Test.java
@@ -1,7 +1,7 @@
 package home.alunoinf.avaliacao.algoritmo17;
 
-        import org.junit.Assert;
-        import org.junit.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class Algoritmo17Test {
 
