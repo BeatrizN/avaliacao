5633e41f67f0b23d96a2ae7ec42ba47b66ea1590
alunoinf <alunoinf@pc-649301.(none)>
2015-11-06 21:49:27 -0200

    Algoritmo 20

--- algoritmo20/src/test/java/home/alunoinf/avaliacao/algoritmo20/Algoritmo20Test.java
+++ algoritmo20/src/test/java/home/alunoinf/avaliacao/algoritmo20/Algoritmo20Test.java
@@ -4,18 +4,22 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class Algoritmo20Test {
+
     @Test(expected = IllegalArgumentException.class)
     public void testPrimeiroParametroInvalido() throws Exception {
         Algoritmo20.algoritmo20(0, 5, 2);
     }
+
     @Test(expected = IllegalArgumentException.class)
     public void testSegundoParametroInvalido() throws Exception {
         Algoritmo20.algoritmo20(3, 0, 2);
     }
+
     @Test(expected = IllegalArgumentException.class)
     public void testTerceiroParametroInvalido() throws Exception {
         Algoritmo20.algoritmo20(3, 2, 0);
     }
+
     @Test
     public void testCasoTrivial() {
         Assert.assertEquals((36/24), Algoritmo20.algoritmo20(10, 2, 3));
