e1209d0dc1c0c4d1739e0721c76c48215b63e6ba
alunoinf <alunoinf@pc-649301.(none)>
2015-11-10 17:23:36 -0200

    Algoritmo 39

---
+++ algoritmo39/src/test/java/home/alunoinf/avaliacao/algoritmo39/Algoritmo39Test.java
@@ -0,0 +1,12 @@
+package home.alunoinf.avaliacao.algoritmo39;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Algoritmo39Test {
+
+    @Test
+    public void testCasoTrivial() {
+        Assert.assertEquals(4, Algoritmo39.algoritmo39());
+    }
+}
