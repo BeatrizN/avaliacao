26cb204fe8352c1adfe15aa2acef6a64ae1d55dc
alunoinf <alunoinf@pc-649301.(none)>
2015-11-11 14:24:29 -0200

    Algoritmo 35

--- algoritmo35/src/main/java/home/alunoinf/avaliacao/algoritmo35/Algoritmo35.java
+++ algoritmo35/src/main/java/home/alunoinf/avaliacao/algoritmo35/Algoritmo35.java
@@ -1,14 +1,14 @@
 package home.alunoinf.avaliacao.algoritmo35;
 
 /**
- * Classe para calcular LUHN.
+ * Classe para calcular a Fórmula de  LUHN.
  */
 public class Algoritmo35 {
 
     /**
-     * Classe para calcular LUHN.
+     * Classe para calcular a Fórmula de LUHN.
      * @param n Vetor ne tamanho >= 2.
-     * @return
+     * @return O resultado da formula em relaçaõ ao vetor.
      */
     public static int algoritmo35(int[] n) {
         if (n.length < 2) {
@@ -21,12 +21,10 @@ public class Algoritmo35 {
         int l = k - 1; // Remove dígitos verificador do cômputo.
         boolean a = false;
 
-        while(l > 0) {
+        while(l >= 0) {
             if (a) {
                 p += n[l]; // Acumula parcelas ímpares.
-            }
-
-            else {
+            } else {
                 int t = n[l] * 2; // Dobro do elemento de ordem ímpar.
                 i = i + t / 10 + mod(t, 10); // Acumula dígitos de t.
             }
--- algoritmo35/src/test/java/home/alunoinf/avaliacao/algoritmo35/Algoritmo35Test.java
+++ algoritmo35/src/test/java/home/alunoinf/avaliacao/algoritmo35/Algoritmo35Test.java
@@ -12,10 +12,11 @@ public class Algoritmo35Test {
     }
 
     @Test
-    public void testCasoTrivial() {
-        int [] a = { 0, 3, 2, 4, 8 };
-        Assert.assertEquals(2, Algoritmo35.algoritmo35(a));
-        int [] b = { 0, 5, 2, 2, 4, 1, 2 };
-        Assert.assertEquals(6, Algoritmo35.algoritmo35(b));
+    public void testCasosTriviais() {
+        int [] a = { 1, 3, 2, 4, 8 };
+        Assert.assertEquals(10, Algoritmo35.algoritmo35(a));
+
+        int [] b = { 7, 5, 2, 2, 4, 1, 2 };
+        Assert.assertEquals(1, Algoritmo35.algoritmo35(b));
     }
 }
