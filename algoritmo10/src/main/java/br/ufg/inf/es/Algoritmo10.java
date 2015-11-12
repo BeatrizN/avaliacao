6e8d19d43fd0612619e88d1ecbb462593b2a9216
alunoinf <alunoinf@pc-649301.(none)>
2015-11-12 21:40:04 -0200

    Algoritmo 10

---
+++ algoritmo10/src/main/java/br/ufg/inf/es/Algoritmo10.java
@@ -0,0 +1,44 @@
+package br.ufg.inf.es;
+
+/**
+ * Classe para implementação do crivo de Eratóstenes.
+ */
+public class Algoritmo10 {
+
+    /**
+     * Classe para implementação do crivo de Eratóstenes.
+     * Se o número for primo recebe o valor 0, senão recebe o valor 1.
+     * @param a Vetor de inteiros.
+     * @param n Numeros de termos do vetor.
+     */
+    public static void algoritmo10(int[] a, int n) {
+        if (n < 2) {
+            throw new IllegalArgumentException("Número menor que dois");
+        }
+
+        if (a.length < (n + 1)) {
+            throw new IllegalArgumentException("Tamanho do vetor");
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if (a[i] != 0) {
+                throw new IllegalArgumentException("Número menor que zero");
+            }
+        }
+
+        int i = 2;
+
+        while(i <= n/2) {
+            if (a[i] == 0) {
+                int c = i + i;
+
+                while (c <= n) {
+                    a[c] = 1;
+                    c += i;
+                }
+            }
+
+            i++;
+        }
+    }
+}
---
+++ algoritmo10/src/test/java/br/ufg/inf/es/Algoritmo10Test.java
@@ -0,0 +1,47 @@
+package br.ufg.inf.es;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Algoritmo10Test {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testVetorInvalido() throws Exception {
+        int[] a = {2, 7, 6, 10, 8};
+        Algoritmo10.algoritmo10(a, 5);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSegundoParametroInvalido() throws Exception {
+        int a[] = {1, 0, 9, 5};
+        Algoritmo10.algoritmo10(a, 1);
+    }
+
+    @Test
+    public void testQuatroNaoPrimoCincoPrimo() {
+        int[] a = new int[6];
+        Algoritmo10.algoritmo10(a, 5);
+
+        int[] expected = { 0, 0, 0, 0, 1, 0 };
+        Assert.assertArrayEquals(expected, a);
+    }
+
+    @Test
+    public void testUltimoPrimoSendoDezenove() {
+        int[] a = new int[20];
+        Algoritmo10.algoritmo10(a, 19);
+
+        int[] expected = { 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0 };
+        Assert.assertArrayEquals(expected, a);
+    }
+
+    @Test
+    public void testSessentaNaoPrimo() {
+        int[] a = new int[61];
+        Algoritmo10.algoritmo10(a, 60);
+        Assert.assertEquals(1, a[40]);
+        Assert.assertEquals(0, a[17]);
+        Assert.assertEquals(1, a[26]);
+        Assert.assertEquals(0, a[53]);
+    }
+}
